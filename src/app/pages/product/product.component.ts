e574311856f5038aa794d89fea5f2242a750df56
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-09-08 22:55:22 -0300

    queryParams/activaterouter

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
 
+export class ProductComponent implements OnInit {
+    id!: number;
+    nome: string = '';
+    preco: number = 0
+
+    constructor(private activateRoute: ActivatedRoute){
+
+    }
+    ngOnInit(): void {
+        /* this.activateRouter.snapshot.params[id]; */
+        this.id = this.activateRoute.snapshot.params['id'];
+
+        this.nome = this.activateRoute.snapshot.queryParams['nome'];
+    }
 }
